7902952563b5e734cf64f8c8944cc25f27344c99
Ryohei-Funamoto <r.funamoto242@gmail.com>
2022-08-30 12:43:23 +0900

    404ページを修正

--- app/public/wp-content/themes/codeups-inc/404.php
+++ app/public/wp-content/themes/codeups-inc/404.php
@@ -1,13 +1,15 @@
 <?php get_header(); ?>
 
-<div class="l-page-404 page-404">
-  <div class="l-container page-404__container">
-    <h1 class="page-404__title">404&nbsp;not&nbsp;found</h1>
-    <p class="page-404__lead">お探しのページは<br class="u-hidden-pc">見つかりませんでした。</p>
-    <div class="page-404__button">
-      <a href="<?php echo $u_home; ?>" class="c-button">TOPへ</a>
-    </div><!-- /.page-404__button -->
-  </div><!-- /.l-container page-404__container -->
-</div><!-- /.l-page-404 -->
+<main class="l-page-404">
+  <div class="l-page-404__content l-page-404-content page-404">
+    <div class="l-container page-404__container">
+      <h1 class="page-404__title">404&nbsp;not&nbsp;found</h1>
+      <p class="page-404__lead">お探しのページは<br class="u-hidden-pc">見つかりませんでした。</p>
+      <div class="page-404__button">
+        <a href="<?php echo $u_home; ?>" class="c-button">TOPへ</a>
+      </div><!-- /.page-404__button -->
+    </div><!-- /.l-container page-404__container -->
+  </div><!-- /.l-page-404__content l-page-404-content page-404 -->
+</main><!-- /.l-page-404 -->
 
 <?php get_footer(); ?>
