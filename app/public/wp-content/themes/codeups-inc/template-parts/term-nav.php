b97aceb47368b2b7403e2ff275ae871e8f3a4ef4
Ryohei-Funamoto <r.funamoto242@gmail.com>
2022-09-03 15:57:52 +0900

    カレント表示を修正

--- app/public/wp-content/themes/codeups-inc/template-parts/term-nav.php
+++ app/public/wp-content/themes/codeups-inc/template-parts/term-nav.php
@@ -2,7 +2,7 @@
   <div class="l-container p-term-nav__container">
     <ul class="p-term-nav__items">
       <?php if (is_post_type_archive('works') || is_post_type_archive('blog')) : ?>
-        <li class="p-term-nav__item is-active"><a href="<?php echo get_post_type_archive_link($args['post_type']); ?>">ALL</a></li>
+        <li class="p-term-nav__item is-active">ALL</li>
         <?php
         $terms = get_terms($args['tax'], array(
           'order' => 'ASC',
@@ -24,12 +24,12 @@
           'hide_empty' => false,
         ));
         foreach ($terms as $term) :
+          if ($term_name === $term->name) :
         ?>
-          <li class="<?php if ($term_name === $term->name) {
-                        echo 'p-term-nav__item is-active';
-                      } else {
-                        echo 'p-term-nav__item';
-                      } ?>"><a href="<?php echo esc_url(get_term_link($term, $args['tax'])); ?>"><?php echo esc_html($term->name); ?></a></li>
+            <li class="p-term-nav__item is-active"><?php echo esc_html($term->name); ?></li>
+          <?php else : ?>
+            <li class="p-term-nav__item"><a href="<?php echo esc_url(get_term_link($term, $args['tax'])); ?>"><?php echo esc_html($term->name); ?></a></li>
+          <?php endif; ?>
         <?php endforeach; ?>
       <?php endif; ?>
     </ul><!-- /.p-term-nav__items -->
